f6d24c945804c28ef66cda676f1901f0fe7c6e25
Afif Nur Fauzi <alscheift@gmail.com>
2023-07-20 04:15:32 +0700

    Add Carousel

--- js/script.js
+++ js/script.js
@@ -28,3 +28,20 @@ document.querySelectorAll(".navbar--nav li a").forEach((nav)=>{
     });
 });
 
+
+///////////////////////////////////////
+// Slider
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
